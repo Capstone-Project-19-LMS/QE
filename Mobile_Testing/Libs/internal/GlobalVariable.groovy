2583eebef699191a2fea9002893e7f799b550276
samuelnelson_1901584@upi.edu <samuelnelson_1901584@upi.edu>
2022-12-15 13:35:46 +0700

    tambah TC dan objek user regist mobile

--- Mobile_Testing/Libs/internal/GlobalVariable.groovy
+++ Mobile_Testing/Libs/internal/GlobalVariable.groovy
@@ -9,6 +9,11 @@ import com.kms.katalon.core.main.TestCaseMain
  */
 public class GlobalVariable {
      
+    /**
+     * <p></p>
+     */
+    public static Object app_name
+     
 
     static {
         try {
@@ -16,6 +21,7 @@ public class GlobalVariable {
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
             selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters())
     
+            app_name = selectedVariables['app_name']
             
         } catch (Exception e) {
             TestCaseMain.logGlobalVariableError(e)
---
+++ Mobile_Testing/Scripts/Manual/TC_004_Login_Valid/Script1671085137577.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+int x = findTestData('Login').getRowNumbers()
+
+    Mobile.startApplication(GlobalVariable.app_name, false)
+
+    Mobile.tap(findTestObject('intropage/next_intro_Button'), 3)
+
+    Mobile.tap(findTestObject('intropage/next_intro2_Button'), 3)
+
+    Mobile.tap(findTestObject('intropage/skip_page_Button'), 3)
+
+for (i = 1; i <= x; i++) {
+
+    Mobile.tap(findTestObject('loginpage/email_txt'), 3)
+
+    Mobile.setText(findTestObject('loginpage/email_txt'), findTestData('Login').getValue(1, i), 0)
+
+    Mobile.tap(findTestObject('loginpage/password_txt'), 3)
+
+    Mobile.setText(findTestObject('loginpage/password_txt'), findTestData('Login').getValue(2, i), 0)
+
+    Mobile.tap(findTestObject('loginpage/login_Button'), 3)
+
+    Mobile.verifyElementExist(findTestObject('homepage/home_title'), 5)
+
+}
+
+
+
+Mobile.closeApplication()
+
